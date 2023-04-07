28d7fdb7f9ef5fe0da30296591ebb3a825f54141
leoBello <leobello.wd@gmail.com>
2023-04-07 16:40:57 +0200

    improve copy mail

--- src/components/CopyTextToClipboard.jsx
+++ src/components/CopyTextToClipboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export async function copyTextToClipboard(text) {
   if ("clipboard" in navigator) {
@@ -9,8 +9,16 @@ export async function copyTextToClipboard(text) {
 }
 
 export function ClipboardCopy({ copyText }) {
-  // eslint-disable-next-line no-unused-vars  
+  // eslint-disable-next-line no-unused-vars
   const [isCopied, setIsCopied] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  useEffect(() => {
+    // start intervale 2s
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 2000);
+  }, [isClicked]);
 
   // This is the function we wrote earlier
   async function copyTextToClipboard(text) {
@@ -28,9 +36,10 @@ export function ClipboardCopy({ copyText }) {
       .then(() => {
         // If successful, update the isCopied state value
         setIsCopied(true);
+        setIsClicked(true);
         setTimeout(() => {
           setIsCopied(false);
-        }, 1500);
+        }, 2000);
       })
       .catch((err) => {
         console.log(err);
@@ -40,7 +49,12 @@ export function ClipboardCopy({ copyText }) {
   return (
     <div>
       {/* <input className="mail" type="text" value={copyText} readOnly /> */}
-      <div className="mail" onClick={handleCopyClick}><p>{copyText}</p><span><i className="fa fa-copy"></i></span></div>
+      <div className="mail" onClick={handleCopyClick}>
+        <p>{!isClicked ? copyText : "Mail copié ! "}</p>
+        { !isClicked && <span>
+          <i className="fa fa-copy"></i>
+        </span>}
+      </div>
       {/* Bind our handler function to the onClick button property */}
     </div>
   );
