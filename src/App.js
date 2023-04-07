78242ad60361a46a8ea9e354490706a836501454
leoBello <leobello.wd@gmail.com>
2023-04-08 01:22:10 +0200

    100 vh bug

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import Contact from './components/Contact';
 import './App.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
+function setWindowHeight() {
+  const windowHeight = window.innerHeight;
+  document.documentElement.style.setProperty('--window-height', `${windowHeight}px`);
+}
+window.addEventListener('load', setWindowHeight);
+window.addEventListener('resize', setWindowHeight);
+
 function App() {
   return (
     <div className="App">
