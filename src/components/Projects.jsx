bdf5f8937f0cbcc3f5b91494662e2647aafbf50e
leoBello <leobello.wd@gmail.com>
2023-04-08 02:08:09 +0200

    ++

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-const ProjectCard = ({ image, title, description, onClick }) => (
+const ProjectCard = ({ image, title, description, onClick, links }) => (
   <div className="project-card" onClick={onClick}>
     <img src={image} alt={title} />
     <div className="project-info">
       <h3>{title}</h3>
       <p>{description}</p>
+      {links && links.map(link => {
+        return (
+          <p><a className="project-link" href={link.target} target="_blank" rel="noopener noreferrer">{link.name}</a></p>
+        )
+      })}
     </div>
   </div>
 );
@@ -15,6 +20,16 @@ const Projects = () => {
     // Ici, vous pouvez gérer les actions à effectuer lors du clic sur une carte
     console.log("Carte cliquée");
   };
+
+  const cpLinks = [{
+    name: "Edition 2022",
+    target: "https://cp2022-web-site.vercel.app/"
+  },
+  {
+    name: "Edition 2023",
+    target: "https://www.chateauperche.com/"
+  }];
+
 
   return (
     <section id="projects" className="projects">
@@ -34,6 +49,13 @@ const Projects = () => {
             title="Amilnote"
             description="Projet de gestions des collaborateurs, des notes de frais et des congés de l'entreprise Amiltone"
             onClick={handleCardClick}
+          />
+          <ProjectCard
+            image="card1.png"
+            title="CP 2022 - 2023"
+            description="2 sites web pour le chateau perché festival"
+            onClick={handleCardClick}
+            links={cpLinks}
           />
           <ProjectCard
             image="card3.png"
