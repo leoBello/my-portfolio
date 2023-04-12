b03f5ec4ea23ee86e563bdf295a504a5dd23f685
leoBello <leobello.wd@gmail.com>
2023-04-12 15:14:24 +0200

    project back

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,6 +17,12 @@ const Projects = () => {
     },
   ];
 
+  const technos = {
+    react: "react.png",
+    nodejs: "nodejs.png",
+    angular: "angular.png",
+  };
+
   return (
     <section id="projects" className="projects">
       <div className="container projects-container">
@@ -29,12 +35,16 @@ const Projects = () => {
             title="Yooz"
             description="Evol et maintance sur un projet de gestion de factures éléctroniques, développement de nouvelles features, Angular / NodeJS"
             onClick={handleCardClick}
+            techno1={technos.angular}
+            techno2={technos.nodejs}
           />
           <ProjectCard
             image="card2-1.png"
             title="Amilnote"
             description="TMA sur un Projet de gestion des collaborateurs, des notes de frais et des congés de l'entreprise Amiltone, React / NodeJS"
             onClick={handleCardClick}
+            techno1={technos.react}
+            techno2={technos.nodejs}
           />
           <ProjectCard
             image="card3-1.png"
@@ -42,12 +52,15 @@ const Projects = () => {
             description="2 sites web pour le chateau perché festival en React : "
             onClick={handleCardClick}
             links={cpLinks}
+            techno1={technos.react}
           />
           <ProjectCard
             image="card4-1.png"
             title="Safran"
             description="Outil de gestion du réseau éléctrique français de l'entreprise RTE, Java EE Angular"
             onClick={handleCardClick}
+            techno1={technos.angular}
+            techno2={technos.nodejs}
           />
         </div>
       </div>
@@ -55,34 +68,51 @@ const Projects = () => {
   );
 };
 
-const ProjectCard = ({ image, title, description, onClick, links }) => (
+const ProjectCard = ({
+  image,
+  title,
+  description,
+  onClick,
+  links,
+  techno1,
+  techno2,
+}) => (
   <div className="card-container" onClick={onClick}>
     <div class="card">
       <div class="front">
         <img src={image} alt={title} />
         <div className="project-info">
-      <h3>{title}</h3>
-      <p>{description}</p>
-      {links &&
-        links.map((link) => {
-          return (
-            <p>
-              <a
-                className="project-link"
-                href={link.target}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {link.name}
-              </a>
-            </p>
-          );
-        })}
-    </div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          {links &&
+            links.map((link) => {
+              return (
+                <p>
+                  <a
+                    className="project-link"
+                    href={link.target}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {link.name}
+                  </a>
+                </p>
+              );
+            })}
+        </div>
       </div>
       <div class="back">
-        <h3>Back of Card</h3>
-        <p>Additional info on the back of the card</p>
+        <h3>Technos</h3>
+        <div className="techno-img-container">
+          {techno1 && (
+            <img src={techno1} alt="techno-logo" className="techno-img" />
+          )}
+          {techno2 && (
+            <img src={techno2} alt="techno-logo" className="techno-img" />
+          )}
+        </div>
+        <h3>Tâches</h3>
+        <p>placeholder</p>
       </div>
     </div>
   </div>
